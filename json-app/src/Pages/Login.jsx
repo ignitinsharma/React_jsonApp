2f71f3ddc3f46f435967039f847a44037aa96d4f
Nitin Sharma <95009807+ignitinsharma@users.noreply.github.com>
2022-12-03 22:06:16 +0530

    Update Login.jsx

--- json-app/src/Pages/Login.jsx
+++ json-app/src/Pages/Login.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { useContext } from 'react'
+import { AuthContext } from '../Context/AuthContext'
 
 const Login = () => {
+    const {isAuth,handleToggle}=useContext(AuthContext)
   return (
-    <div>Login</div>
+    <div>
+
+    {/* 
+    --> IF user is login in So user will access all of this pages else 
+        user can Access only Login or Contact Page
+
+    */}
+
+        <h1>User is {isAuth?"Login":"Logout"} Right now</h1>
+        <button onClick={handleToggle}>{isAuth?"Logout":"Login"}</button>
+    </div>
   )
 }
 
