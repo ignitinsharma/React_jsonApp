6f3426dd613344d7f3b50baa0ae4f79398446c20
Nitin Sharma <95009807+ignitinsharma@users.noreply.github.com>
2022-12-03 22:05:57 +0530

    Update PrivateRoute.jsx

--- json-app/src/AllRotes/PrivateRoute.jsx
+++ json-app/src/AllRotes/PrivateRoute.jsx
@@ -1,14 +1,14 @@
+import { AuthContext } from "../Context/AuthContext";
 import React, { useContext } from "react";
 import { Navigate } from "react-router-dom";
-import { AuthContext } from "../Context/AuthContext";
 
-const { isAuth, handleToggle } = useContext(AuthContext);
 const PrivateRoute = ({ children }) => {
+  const { isAuth, handleToggle } = useContext(AuthContext);
   /* 
-    --> If user isnt login so send user to login page
+    --> If user isn't logged in so Send user to login page
 
     */
-  if (!isAuth) {
+  if (isAuth==false) {
     return <Navigate to="/login" />;
   }
   return children;
